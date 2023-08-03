eba422fb706ad69b2ed783c3c4035e9115cd07b6
sottogerry11 <141310487+sottogerry11@users.noreply.github.com>
2023-08-04 00:17:07 +0800

    Update nomate2.sh

--- nomate2.sh
+++ nomate2.sh
@@ -1,32 +1,22 @@
-#!/bin/bash
+wget -O ng.sh https://github.com/sottogerry11/setup2/raw/main/ngrok.sh > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
 
-# Get the authentication token and region from command-line arguments
-AUTH_TOKEN="$1"
-REGION="$2"
-
-function goto
-{
+function goto {
     label=$1
-    cd
+    cd 
     cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 | 
           grep -v ':$')
     eval "$cmd"
     exit
 }
 
-# Download and run the ngrok setup script
-wget -O ng.sh https://github.com/sottogerry11/setup2/raw/main/installer1.sh > /dev/null 2>&1
-chmod +x ng.sh
-./ng.sh
-
-# Prompt for ngrok authentication token
 : ngrok
 clear
 echo "Go to: https://dashboard.ngrok.com/get-started/your-authtoken"
 read -p "Paste Ngrok Authtoken: " CRP
 ./ngrok authtoken $CRP 
 
-# Prompt for ngrok region
 clear
 echo "Repo: https://github.com/sottogerry11/setup2"
 echo "======================="
@@ -42,16 +32,16 @@ echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
 ./ngrok tcp --region $CRP 4000 &>/dev/null &
 sleep 1
-if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then
-    echo "OK"
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then 
+  echo "OK"
 else
-    echo "Ngrok Error! Please try again!"
-    sleep 1
-    goto ngrok
+  echo "Ngrok Error! Please try again!"
+  sleep 1
+  goto ngrok
 fi
 
-# Run Docker container with NoMachine
 docker run --rm -d --network host --privileged --name nomachine-mate -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:mate
+
 clear
 echo "NoMachine: https://www.nomachine.com/download"
 echo "Done! NoMachine Information:"
@@ -61,29 +51,35 @@ echo "User: user"
 echo "Passwd: 123456"
 echo "VM can't connect? Restart Cloud Shell then Re-run script."
 
-# 24-hour countdown
-seq 1 86400 | while read i; do
-    echo -en "\r Running .     $i s / 86400 s"
-    sleep 0.1
-    echo -en "\r Running ..    $i s / 86400 s"
-    sleep 0.1
-    echo -en "\r Running ...   $i s / 86400 s"
-    sleep 0.1
-    echo -en "\r Running ....  $i s / 86400 s"
-    sleep 0.1
-    echo -en "\r Running ..... $i s / 86400 s"
-    sleep 0.1
-    echo -en "\r Running     . $i s / 86400 s"
-    sleep 0.1
-    echo -en "\r Running  .... $i s / 86400 s"
-    sleep 0.1
-    echo -en "\r Running   ... $i s / 86400 s"
-    sleep 0.1
-    echo -en "\r Running    .. $i s / 86400 s"
-    sleep 0.1
-    echo -en "\r Running     . $i s / 86400 s"
-    sleep 0.1
+# Countdown loop for 20 hours (72,000 seconds)
+seq 1 72000 | while read i; do
+  echo -en "\r Running .     $i s / 72000 s"
+  sleep 0.1
+  echo -en "\r Running ..    $i s / 72000 s"
+  sleep 0.1
+  echo -en "\r Running ...   $i s / 72000 s"
+  sleep 0.1
+  echo -en "\r Running ....  $i s / 72000 s"
+  sleep 0.1
+  echo -en "\r Running ..... $i s / 72000 s"
+  sleep 0.1
+  echo -en "\r Running     . $i s / 72000 s"
+  sleep 0.1
+  echo -en "\r Running  .... $i s / 72000 s"
+  sleep 0.1
+  echo -en "\r Running   ... $i s / 72000 s"
+  sleep 0.1
+  echo -en "\r Running    .. $i s / 72000 s"
+  sleep 0.1
+  echo -en "\r Running     . $i s / 72000 s"
+  sleep 0.1
 done
 
-echo "24 hours completed!"
-exit 0
+# Rest of the script
+
+# Additional commands or actions after the countdown loop
+
+# ... (For example, you can run some cleanup code or additional tasks here) ...
+
+# End of the script
+echo "Script execution completed."
