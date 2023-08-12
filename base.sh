804122521861619c0cae5101b35b44953ef483b2
sottogerry11 <141310487+sottogerry11@users.noreply.github.com>
2023-08-12 22:13:44 +0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -1,1 +1,11 @@
-New content of the file
+#!/bin/bash
+
+# Go
+
+echo "Away"
+
+sudo sed -i "s/ClientAliveInterval 0/ClientAliveInterval 0/g" /etc/ssh/sshd_config
+sudo sed -i "s/ClientAliveCountMax 3/ClientAliveCountMax 0/g" /etc/ssh/sshd_config
+sudo service ssh restart
+
+echo "Gone now."
